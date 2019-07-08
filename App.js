0f3b24ba8e2bc8e4d4c4a5e3eed96476ec14935a
Akram11 <akram.f11@gmail.com>
2019-07-08 20:22:49 +0200

    showing initial data and icons

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AddEntry from './components/AddEntry'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>oppennning app.js to start working on your app!</Text>
+       <AddEntry />
+       <Text>da fuq1</Text>
     </View>
   );
 }
