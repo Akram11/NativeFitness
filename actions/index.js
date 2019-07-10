3f6e44372cc17e35350cfb2b7baed3c0a42ff190
Akram11 <akram.f11@gmail.com>
2019-07-10 14:09:05 +0200

    created an actions file

--- actions/index.js
+++ actions/index.js
@@ -1,9 +1,16 @@
 export const RECEIVE_ENTRIES = "RECEIVE_ENTRIES ";
 export const ADD_ENTRY = " ADD_ENTRY ";
 
-export function reveiveEntries(entries) {
+export function receiveEntries(entries) {
   return {
     type: RECEIVE_ENTRIES,
     entries
   };
+}
+
+export function AddEntry(entry){
+    return {
+        type: ADD_ENTRY,
+        entry
+    }
 }
